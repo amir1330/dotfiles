d8d9baf5c8744c66d627fbc3d25f958f92b6f401
amir <iskakovamir7486@gmail.com>
2026-07-23 14:51:26 +0500

    Initial dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+
+# overwrite greeting
+# potentially disabling fastfetch
+function fish_greeting
+    #date
+end
+
+
+starship init fish | source
+
+# kimi-code
+fish_add_path -g "/home/amir1330/.kimi-code/bin"
